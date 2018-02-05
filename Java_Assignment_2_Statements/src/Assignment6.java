4e63390d62418e564c6d00b42154f855ed3d0263
Ricky Brown <rickyb4536@gmail.com>
2018-02-05 13:44:22 -0500

    Third Commit
    Adding assignment 6. Goes through each statement to see which one is
    true and then prints something out.

---
+++ Java_Assignment_2_Statements/src/Assignment6.java
@@ -0,0 +1,22 @@
+
+public class Assignment6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float x;
+		x = 10.5f;
+		
+		if (x == 3){
+			System.out.println("x is equal to 3");
+		}
+		else if (x > 5){
+			System.out.println("x is greater than 5");
+		}
+		else if (x <= 0){
+			System.out.println("x is less than or equal to 0");
+		}else {
+			System.out.println("x is none of the options");
+			}
+	}
+
+}
