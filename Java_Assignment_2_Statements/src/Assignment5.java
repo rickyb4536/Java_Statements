f2fdbad3dddfda8b8876dc7f72fd3b9ed294d342
Ricky Brown <rickyb4536@gmail.com>
2018-02-05 13:07:42 -0500

    Second Commit

--- Java_Assignment_2_Statements/src/Assignment5.java
+++ Java_Assignment_2_Statements/src/Assignment5.java
@@ -3,7 +3,14 @@ public class Assignment5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int x;
+		x = 10;
+		
+		if (x == 3) {
+			System.out.println("x is equal to 3");
+		}else {
+			System.out.println("x is not equal to 3");
+		}
 	}
 
 }
